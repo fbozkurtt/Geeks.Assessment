f81d3151ff1bed4f674ca52dbfa62ceb16bfccaa
Furkan Bozkurt <furkanbozkurt55@gmail.com>
2022-06-04 02:22:43 +0300

    Add project files & gitignore

---
+++ Assessment.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assessment
+{
+    public class Assessment : IAssessment
+    {
+        #region Methods
+
+#nullable enable
+        /// <summary>
+        /// Returns the score with the highest value
+        /// </summary>
+        public Score? WithMax(IEnumerable<Score> scores)
+        {
+            return scores.Max();
+        }
+#nullable disable
+
+        /// <summary>
+        /// Returns the average value of the collection. For an empty collection it returns null
+        /// </summary>
+        public double? GetAverageOrDefault(IEnumerable<int> items)
+        {
+            var itemsList = items?.ToList();
+            var nullOrEmpty = items is null || itemsList.Any() is false;
+
+            if (nullOrEmpty)
+                return null;
+            return itemsList.Average();
+        }
+
+
+        /// <summary>
+        /// Appends the suffix value to the text if the text has value. If not, it returns empty.
+        /// </summary>
+        public string WithSuffix(string text, string suffixValue)
+        {
+            if (text is null)
+                return string.Empty;
+            return string.Concat(text, suffixValue);
+        }
+
+        /// <summary>
+        /// It fetches all the data from the source.
+        /// </summary>
+        /// <param name="source">The source data provider returns items by page. NextPageToken is the page token of the next page. If there are no more items to return, nextPageToken will be empty. Passing a null or empty string to the provider will return the first page of the data.
+        /// If no value is specified for nextPageToken, the provider will return the first page.
+        /// </param>
+        /// <returns></returns>
+        public IEnumerable<Score> GetAllScoresFrom(IDataProvider<Score> source)
+        {
+            var scores = new List<Score>();
+
+            IDataProvider<Score>.IDataProviderResponse response = null;
+
+            do
+            {
+                response = source.GetData(response?.NextPageToken);
+                scores.AddRange(response.Items);
+            } while (string.IsNullOrWhiteSpace(response.NextPageToken) is false);
+
+            return scores;
+        }
+
+        /// <summary>
+        /// Returns child's name prefixed with all its parents' names separated by the specified separator.Example : Parent/Child
+        /// </summary>
+        public string GetFullName(IHierarchy child, string separator = null)
+        {
+            separator ??= "/";
+
+            if (child.Parent is null)
+                return child.Name;
+            return string.Concat(GetFullName(child.Parent, separator), separator, child.Name);
+        }
+
+        /// <summary>
+        /// Refactor: Returns the value that is closest to the average value of the specified numbers.
+        /// </summary>
+        public int? ClosestToAverageOrDefault(IEnumerable<int> numbers)
+        {
+            var numberArray = numbers as int[] ?? numbers.ToArray();
+            var average = numberArray.Average();
+
+            var result = numberArray
+                .Select(n => new {Value = n, Distance = Math.Abs(average - n)})
+                .GroupBy(g => g.Distance)
+                .OrderBy(g => g.Key)
+                .ToList();
+
+            if (result.FirstOrDefault()?.Distinct().Count() > 1)
+                return null;
+
+            return result.FirstOrDefault()?.FirstOrDefault()?.Value;
+        }
+
+        /// <summary>
+        /// Returns date ranges that have similar bookings on each day in the range.
+        /// Read the example carefully.
+        /// Example : [{Project:HR, Date: 01/02/2020 , Allocation: 10},
+        ///            {Project:CRM, Date: 01/02/2020 , Allocation: 15},
+        ///            {Project:HR, Date: 02/02/2020 , Allocation: 10},
+        ///            {Project:CRM, Date: 02/02/2020 , Allocation: 15},
+        ///            {Project:HR, Date: 03/02/2020 , Allocation: 15},
+        ///            {Project:CRM, Date: 03/02/2020 , Allocation: 15},
+        ///            {Project:HR, Date: 04/02/2020 , Allocation: 15},
+        ///            {Project:CRM, Date: 04/02/2020 , Allocation: 15},
+        ///            {Project:HR, Date: 05/02/2020 , Allocation: 15},
+        ///            {Project:CRM, Date: 05/02/2020 , Allocation: 15},
+        ///            {Project:ECom, Date: 05/02/2020 , Allocation: 15},
+        ///            {Project:ECom, Date: 06/02/2020 , Allocation: 10},
+        ///            {Project:CRM, Date: 06/02/2020 , Allocation: 15}
+        ///            {Project:ECom, Date: 07/02/2020 , Allocation: 10},
+        ///            {Project:CRM, Date: 07/02/2020 , Allocation: 15}]    
+        /// Returns : 
+        ///          [
+        ///            { From:01/02/2020 , To:02/02/2020 , [{ Project:CRM , Allocation:15 },{ Project:HR , Allocation:10 }]  },
+        ///            { From:03/02/2020 , To:04/02/2020 , [{ Project:CRM , Allocation:15 },{ Project:HR , Allocation:15 }]  },
+        ///            { From:05/02/2020 , To:05/02/2020 , [{ Project:CRM , Allocation:15 },{ Project:HR , Allocation:15 },{ Project:ECom , Allocation:15 }]  },
+        ///            { From:06/02/2020 , To:07/02/2020 , [{ Project:CRM , Allocation:15 },{ Project:ECom , Allocation:10 }]  }
+        ///          ]
+        /// </summary>
+        public IEnumerable<BookingGrouping> Group(IEnumerable<Booking> dates)
+        {
+            return dates
+                .GroupBy(b => b.Date.Date) // Grouping by date so we can group by similar bookings 
+                .GroupBy(dg =>
+                    CalculateHash(dg.ToList())) // Grouping by similar bookings by using a custom hash function
+                .Select(g =>
+                {
+                    return new BookingGrouping()
+                    {
+                        From = g.First().Key,
+                        To = g.Last().Key,
+                        Items = g.First().Select(b => new BookingGroupingItem() // Taking only the first grouping
+                            // because similar bookings already grouped
+                            // so they consists of the same bookings
+                            {
+                                Allocation = b.Allocation,
+                                Project = b.Project
+                            }).ToList()
+                    };
+                }).ToList();
+        }
+
+        /// <summary>
+        /// Merges the specified collections so that the n-th element of the second list should appear after the n-th element of the first collection. 
+        /// Example : first : 1,3,5 second : 2,4,6 -> result : 1,2,3,4,5,6
+        /// </summary>
+        public IEnumerable<int> Merge(IEnumerable<int> first, IEnumerable<int> second)
+        {
+            var firstArray = first as int[] ?? first.ToArray();
+            var secondArray = second as int[] ?? second.ToArray();
+
+            if (firstArray.Length != secondArray.Length)
+                throw new InvalidOperationException("Collection sizes do not match.");
+
+            var mergedArray = new List<int>();
+
+            for (var i = 0; i < firstArray.Length; i++)
+            {
+                mergedArray.Add(firstArray[i]);
+                mergedArray.Add(secondArray[i]);
+            }
+
+            return mergedArray.AsEnumerable();
+        }
+
+        #endregion
+
+        #region Utilities
+
+        private static int CalculateHash(List<Booking> items)
+        {
+            items = items.OrderBy(b => b.Allocation.GetHashCode() + b.Project.GetHashCode()).ToList();
+            const int seed = 487;
+            const int modifier = 31;
+
+            unchecked
+            {
+                return items.Aggregate(seed, (current, item) =>
+                    (current * modifier) + item.Allocation.GetHashCode() + item.Project.GetHashCode());
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Assessment
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var assessment = new Assessment();
+
+            #region WithMax
+
+            Console.WriteLine("WithMax");
+
+            Console.WriteLine(assessment.WithMax(
+                    Enumerable.Range(1, 10)
+                        .Select(x => new Score {Value = x * x}))?.Value.ToString()
+            );
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region GetAverageOrDefault
+
+            Console.WriteLine("GetAverageOrDefault");
+
+            Console.WriteLine(assessment.GetAverageOrDefault(Enumerable.Range(1, 6)).ToString());
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region WithSuffix
+
+            Console.WriteLine("WithSuffix");
+
+            Console.WriteLine(assessment.WithSuffix("CORRECT_TO", "M_U_N_D_O"));
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region GetFullName
+
+            Console.WriteLine("GetFullName");
+
+            var child = new Hierarchy("John",
+                new Hierarchy("Jane",
+                    new Hierarchy("Joe",
+                        new Hierarchy("Jolene",
+                            new Hierarchy("Josh",
+                                new Hierarchy("Jake"))))));
+
+            Console.WriteLine(assessment.GetFullName(child));
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region ClosestToAverageOrDefault
+
+            Console.WriteLine("ClosestToAverageOrDefault");
+
+            Console.WriteLine(assessment.ClosestToAverageOrDefault(Enumerable.Range(1, 5)).ToString());
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region Group
+
+            Console.WriteLine("Group");
+
+            var bookingList = new List<Booking>()
+            {
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("01/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "HR",
+                    Date = DateTime.ParseExact("01/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 10
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("02/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "HR",
+                    Date = DateTime.ParseExact("02/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 10
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("03/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "HR",
+                    Date = DateTime.ParseExact("03/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("04/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "HR",
+                    Date = DateTime.ParseExact("04/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("05/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "HR",
+                    Date = DateTime.ParseExact("05/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "ECom",
+                    Date = DateTime.ParseExact("05/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("06/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "ECom",
+                    Date = DateTime.ParseExact("06/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 10
+                },
+                new Booking()
+                {
+                    Project = "CRM",
+                    Date = DateTime.ParseExact("07/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 15
+                },
+                new Booking()
+                {
+                    Project = "ECom",
+                    Date = DateTime.ParseExact("07/02/2020", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Allocation = 10
+                }
+            };
+
+            var bookingGroups = assessment.Group(bookingList);
+
+            foreach (var bookingGroup in bookingGroups)
+            {
+                Console.Write($"From: {bookingGroup.From.ToShortDateString()}," +
+                              $" To:{bookingGroup.To.ToShortDateString()}");
+                foreach (var bookingGroupingItem in bookingGroup.Items)
+                {
+                    Console.Write($", 'Project:{bookingGroupingItem.Project}," +
+                                  $" Allocation:{bookingGroupingItem.Allocation.ToString(CultureInfo.InvariantCulture)}'");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            #endregion
+
+            #region Merge
+
+            Console.WriteLine("Merge");
+
+            var mergedArray = assessment.Merge(new[] {1, 3, 5}, new[] {2, 4, 6});
+
+            Console.WriteLine(string.Join(",", mergedArray));
+
+            Console.WriteLine();
+
+            #endregion
+        }
+    }
+
+    internal class Hierarchy : IHierarchy
+    {
+        public IHierarchy Parent { get; }
+        public string Name { get; }
+
+        public Hierarchy(string name, IHierarchy parent = null)
+        {
+            Parent = parent;
+            Name = name;
+        }
+    }
+}
